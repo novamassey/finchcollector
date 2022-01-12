82e1a436363168e4a0a0e738bd3661795fd44508
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2022-01-11 18:53:21 -0800

    delete database

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 #import reverse to use in the get_absolute_url function
 from django.urls import reverse
+# from django.utils import datetime
+
+
+MEALS = (
+    ('B', 'Bird Breakfast'),
+    ('L', 'Birdy Lunch'),
+    ('S', 'Baby Bird Snack'),
+    ('D', 'Big Bird Dinner')
+)
+
+FOODS = (
+    ('M', 'mealworms'),
+    ('S', 'seeds'),
+    ('V', 'fresh veggies'),
+    ('E', 'eggfood')
+)
 
 # Create your models here.
 
@@ -27,5 +43,21 @@ class Finch(models.Model):
         return f"({self.id} - {self.name}"
 
     def get_absolute_url(self):
-        return reverse('finches_detail', kwargs={'finch_id': self.id})    
+        return reverse('finches_detail', kwargs={'finch_id': self.id}) 
+
+class Feeding(models.Model):
+    date = models.DateField('Feed Date')
+    meal = models.CharField(
+        max_length=1,
+        choices = MEALS,
+        default = MEALS[0][0]
+        )
+    food = models.CharField(
+        max_length=1,
+        choices = FOODS,
+        default = FOODS[1][0]) 
+    finch = models.ForeignKey(Finch, on_delete = models.CASCADE)    
+
+    def __str__(self):
+        return f"A {self.get_meal_display()} of {self.get_food_display()} on {self.date}"          
 
